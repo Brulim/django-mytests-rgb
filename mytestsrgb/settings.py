d16191dadca184543fe2bc962b242141d3b3e66c
Bruno Lima <arquitetobrunolima@gmail.com>
2022-10-28 11:49:47 -0300

    corrigindo segurança

--- mytestsrgb/settings.py
+++ mytestsrgb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o2av_x84%*_(wk366_fu%knsz^xpa7e)(&0j5(@e&7q%p)2-bv'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
